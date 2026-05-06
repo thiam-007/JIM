b66dcdfedd6eda35de928146c1e5cc6d5e557c0e
thiam007 <cheickahmedthiam07@gmail.com>
2026-05-06 12:33:15 +0000

    add the partners logos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-a24bf94b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5vfvrce53q8"
+    "revision": "0.0htunok5418"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
