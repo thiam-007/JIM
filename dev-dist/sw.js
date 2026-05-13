dce1ca4f5ab6f51e309a2970ca83cb5e01f46f34
thiam007 <cheickahmedthiam07@gmail.com>
2026-05-13 08:24:03 +0000

    Installing on laptop

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-a24bf94b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i2e1pupe15o"
+    "revision": "0.19n4jerisag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
